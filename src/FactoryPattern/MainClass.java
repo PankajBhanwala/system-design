a50d90588b0f7c8bbd97542a42063ef8b0a5241f
Hello-Pankaj <pankajbhanwalasnp@gmail.com>
2025-10-07 15:02:48 +0530

    Factory Pattern Added

---
+++ src/FactoryPattern/MainClass.java
@@ -0,0 +1,11 @@
+package FactoryPattern;
+
+public class MainClass {
+    public static void main(String args[]) {
+        ShapeFactory shapeFactory = new ShapeFactory();
+        Shape circle = shapeFactory.getShape("CIRCLE");
+        circle.draw();
+        Shape rectangle = shapeFactory.getShape("RECTANGLE");
+        rectangle.draw();
+    }
+}
